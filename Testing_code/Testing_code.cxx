7a36de8d36dc56be1197bf5789f81b5972929fc4
Old-Lx <92254970+Old-Lx@users.noreply.github.com>
2022-07-17 08:49:21 -0400

    Delete Testing_code

--- Testing_code/Testing_code.cxx
+++
@@ -1,7 +0,0 @@
-///Este es un programa creado por Luis Becerra para probar 
-///conceptos, funciones or anything tha could be tested in order
-///to achieve my programming goals =D
-
-#include <stdio.h>
-#include <string.h>
-
